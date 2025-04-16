bb8ec3725ca418510118ed90a14052198fb095bc
rusumvlad <vlad.rusu@student.upt.ro>
2025-04-16 23:50:34 +0300

    Increased likes on a single gallery item

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,67 +1,35 @@
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import data from './data/photos.json';
-import './Gallery.css';
-import Title from './Title';
-
-const getLikesFromStorage = () => {
-  const likes = localStorage.getItem('likes');
-  return likes ? JSON.parse(likes) : {};
-};
+import { useEffect, useState } from "react";
+import data from "./data/photos.json";
+import "./Gallery.css";
+import Title from "./Title";
+import GalleryCard from "./GalleryCard/GalleryCard";
 
 function Gallery() {
-  const photos = data.fotos;
-  const [likes, setLikes] = useState(getLikesFromStorage);
+  const [photos, setPhotos] = useState([]);
 
-  const handleLike = (photoId) => {
-    setLikes((prev) => {
-      const newLike = { ...prev, [photoId]: (prev[photoId] || 0) + 1 };
-      return newLike;
-    });
+  const handleIncreaseLike = (photoId) => {
+    setPhotos((prev) =>
+      prev.map((photo) =>
+        photo.id === photoId ? { ...photo, like: photo.like + 1 } : photo
+      )
+    );
   };
 
   useEffect(() => {
-    localStorage.setItem('likes', JSON.stringify(likes));
-  }, [likes]);
+    setPhotos(data.fotos);
+  }, []);
 
   return (
     <>
       <Title />
       <div className="container">
         <div className="row">
-          {photos.map((photo) => (
-            <div key={photo.id} className="col-md-4 mb-4">
-              <div className="card gallery-card h-100">
-                <div className="img-wrapper">
-                  <img
-                    src={photo.srcThumbnail}
-                    className="card-img-top"
-                    alt={photo.title}
-                  />
-                </div>
-                <div className="card-body d-flex flex-column justify-content-between">
-                  <div>
-                    <h5 className="card-title">{photo.title}</h5>
-                    <div className="card-text description">
-                      {' '}
-                      {photo.shortDesc}
-                    </div>
-                  </div>
-                  <div className="like-section">
-                    <button
-                      className="btn btn-like"
-                      onClick={() => handleLike(photo.id)}
-                    >
-                      👍 Like
-                    </button>
-                    <span>{likes[photo.id]} likes</span>
-                  </div>
-                  <Link to={`/photo/${photo.id}`}>
-                    <button className="btn btn-see-more mt-2">See More</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+          {photos?.map((photo) => (
+            <GalleryCard
+              key={photo.id}
+              photo={photo}
+              handleIncreaseLike={handleIncreaseLike}
+            />
           ))}
         </div>
       </div>
---
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router";
+
+const GalleryCard = ({ photo, handleIncreaseLike }) => {
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card gallery-card h-100">
+        <div className="img-wrapper">
+          <img
+            src={photo.srcThumbnail}
+            className="card-img-top"
+            alt={photo.title}
+          />
+        </div>
+        <div className="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h5 className="card-title">{photo.title}</h5>
+            <div className="card-text description"> {photo.shortDesc}</div>
+          </div>
+          <div className="like-section">
+            <button
+              className="btn btn-like"
+              onClick={() => handleIncreaseLike(photo.id)}
+            >
+              👍 Like
+            </button>
+            <span>{photo.like} likes</span>
+          </div>
+          <Link to={`/photo/${photo.id}`}>
+            <button className="btn btn-see-more mt-2">See More</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryCard;
